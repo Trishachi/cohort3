911c6fb7bbc7e3b066d6ffc4912fffb8692b71be
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2019-10-20 22:46:06 -0600

    Added Card Numbering - Work in Progress

--- src/02-dom/scripts/functions.test.js
+++ src/02-dom/scripts/functions.test.js
@@ -1,5 +1,12 @@
 import functions from "./functions.js";
 
 test("displays new list item after a click", () => {
+  // Set up our document body
+  //   document.body.innerHTML =
+  //     "<div>" +
+  //     '  <span id="username" />' +
+  //     '  <button id="button" />' +
+  //     "</div>";
+
   expect(functions.addListOnBtnClick).toBeCalled();
 });
