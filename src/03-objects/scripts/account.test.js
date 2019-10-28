8777cea64b3d7fa241752929857100f270bc2359
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2019-10-28 16:25:42 -0600

    Comp 130A Completed

--- src/03-objects/scripts/account.test.js
+++ src/03-objects/scripts/account.test.js
@@ -2,7 +2,12 @@ import { Account } from "./account.js";
 
 test("Test for Account Initializations", () => {
   let newAcc = new Account("Checking", 20);
+  console.log(`Current Account Balance: ${newAcc.balance()}`);
+  expect(newAcc.balance()).toBe(20);
   newAcc.deposit(10);
-  console.log(newAcc.balance());
   expect(newAcc.balance()).toBe(30);
+  console.log(`Current Account Balance: ${newAcc.balance()}`);
+  newAcc.withdraw(20);
+  console.log(`Current Account Balance: ${newAcc.balance()}`);
+  expect(newAcc.balance()).toBe(10);
 });
