3baeaa5ee704cd7ca93d89f3d12e643c3806be2b
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2019-11-03 21:16:17 -0700

    Cleaned up account test code

--- src/03-objects/scripts/account.test.js
+++ src/03-objects/scripts/account.test.js
@@ -3,15 +3,12 @@ import helperFunctions from "./helperFunctions.js";
 
 test("Test for Account Initializations", () => {
   let newAcc = new Account("Checking", 20);
-  console.log(`Current Account Balance: ${newAcc.balance()}`);
   expect(newAcc.balance()).toBe(20);
   newAcc.deposit(10);
   expect(newAcc.balance()).toBe(30);
-  console.log(`Current Account Balance: ${newAcc.balance()}`);
   newAcc.deposit(-1);
   expect(newAcc.balance()).toBe(30);
   newAcc.deposit(50);
-  console.log(`Current Account Balance: ${newAcc.balance()}`);
   expect(newAcc.balance()).toBe(80);
   newAcc.withdraw(50);
   expect(newAcc.balance()).toBe(30);
@@ -21,7 +18,6 @@ test("Test for Account Initializations", () => {
 
 test("Testing Account Controller", () => {
   let newAccount = new AccountController();
-  // console.log(newAccount.addAccount("New", 10));
   expect(newAccount.addAccount("New", 10)).toEqual([
     { accountName: "New", AccBalance: 10 }
   ]);
@@ -30,22 +26,20 @@ test("Testing Account Controller", () => {
     { accountName: "New", AccBalance: 10 },
     { accountName: "New1", AccBalance: 20 }
   ]);
-  // console.log(newAccount.accountHolder);
+
   expect(newAccount.removeAccount("New")).toEqual([
     { accountName: "New1", AccBalance: 20 }
   ]);
-  console.log(newAccount.accountHolder);
+
   newAccount.addAccount("New2", 30);
   newAccount.addAccount("New3", 15);
-  console.log(newAccount.accountHolder);
   expect(newAccount.getAccBalances(newAccount.accountHolder)).toEqual([
     20,
     30,
     15
   ]);
-  console.log(newAccount.getAccBalances(newAccount.accountHolder));
+
   expect(newAccount.totalAccBalance(newAccount.accountHolder)).toEqual(65);
-  console.log(newAccount.totalAccBalance(newAccount.accountHolder));
   expect(newAccount.highestValAcc(newAccount.accountHolder)).toEqual(30);
   expect(newAccount.lowestValAcc(newAccount.accountHolder)).toEqual(15);
   expect(newAccount.accountList.length).toEqual(3);
@@ -69,8 +63,6 @@ test("Check Card Funtions", () => {
   let newCard1 = helperFunctions.addCard("Sav", 200, div);
   expect(div.childElementCount).toBe(elementCount + 1);
   let newCard2 = helperFunctions.addCard("Sav", 200, div);
-  console.log(div.childElementCount);
   helperFunctions.deleteCard(newCard2);
-  console.log(div.childElementCount);
   expect(div.childElementCount).toBe(1);
 });
