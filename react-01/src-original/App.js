ec9f1b002d337c2ed912948cc95f40fc13f1535a
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2019-11-11 22:01:57 -0700

    React App WIP

--- react-01/src-original/App.js
+++ react-01/src-original/App.js
@@ -1,13 +1,12 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>I am in control of this application and my name is Chisom</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
