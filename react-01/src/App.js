e1b02b73192e6365820fb60d8e22e5123c9df791
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2019-11-14 22:55:41 -0700

    Intro to React State Completed

--- react-01/src/App.js
+++ react-01/src/App.js
@@ -2,26 +2,50 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>I am in control of this application and my name is Chisom</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.counter = 21;
+    this.state = {
+      myState: "TBD" //Initialization of the state
+    };
+  }
+
+  onPushMe = () => {
+    console.log("You pushed me");
+    this.counter++;
+    console.log(this.counter);
+    this.setState({
+      myState: "now" + this.counter
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>
+            I am in control of this application and my name is Chisom
+            {this.counter}
+            {this.state.myState}
+          </h1>
+          <button onClick={this.onPushMe}>Push Me</button>
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
