5b0ba588ed69c3f0731d2096c19233149c27418b
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2019-11-15 14:59:03 -0700

    Conditional rendering set up

---
+++ react-01/src/components/EvenComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const EvenComponent = props => {
+  return (
+    <div>
+      <h1>Even Component</h1>
+    </div>
+  );
+};
+
+export default EvenComponent;
---
+++ react-01/src/components/OddComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const OddComponent = props => {
+  return (
+    <div>
+      <h1>Odd Component</h1>
+    </div>
+  );
+};
+
+export default OddComponent;
