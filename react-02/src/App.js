b10e75761f0db1b39045a84acec96c5e14a0afc7
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2019-11-17 00:07:28 -0700

    SGV Animations

--- react-02/src/App.js
+++ react-02/src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <Animation />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo animate" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
--- react-02/src/components/Animations/svgComponent.js
+++ react-02/src/components/Animations/svgComponent.js
@@ -5,11 +5,11 @@ import "./svgComponent.css";
 
 const Animation = () => {
   return (
-    <div className="Navigation">
-      <img src={welcome} className="svg-image" alt="svg1" />
-      <img src={tictactoe} className="svg-image" alt="svg2" />
-      <img src={welcome} className="svg-image" alt="svg3" />
-      <img src={tictactoe} className="svg-image" alt="svg4" />
+    <div id="Header" className="Navigation">
+      <img src={welcome} className="svg-image animate" alt="svg1" />
+      <img src={tictactoe} className="svg-image animate1" alt="svg2" />
+      <img src={welcome} className="svg-image animate2" alt="svg3" />
+      <img src={tictactoe} className="svg-image animate3" alt="svg4" />
     </div>
   );
 };
