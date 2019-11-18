f8a416104c19a1999a29937aec2b13bf55a3335c
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2019-11-17 22:17:07 -0700

    SVG Navigation Completed

--- react-02/src/App.js
+++ react-02/src/App.js
@@ -3,26 +3,51 @@ import logo from "./logo.svg";
 import "./App.css";
 import Animation from "./components/Animations/svgComponent";
 
-function App() {
-  return (
-    <div className="App">
-      <Animation />
-      <header className="App-header">
-        <img src={logo} className="App-logo animate" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      myState: "Display Action"
+    };
+  }
+
+  onMouseOver = () => {
+    this.setState({
+      myState: "Moused Over"
+    });
+  };
+
+  onMouseOut = () => {
+    this.setState({
+      myState: "Display Action"
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Animation
+          mouseOverEvent={this.onMouseOver}
+          mouseOutEvent={this.onMouseOut}
+        />
+        <header className="App-header">
+          <p id="hoverStatus">{this.state.myState}</p>
+          <img src={logo} className="App-logo animate" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
--- react-02/src/components/Animations/svgComponent.js
+++ react-02/src/components/Animations/svgComponent.js
@@ -3,13 +3,37 @@ import welcome from "./welcome.svg";
 import tictactoe from "./tictactoe.svg";
 import "./svgComponent.css";
 
-const Animation = () => {
+const Animation = props => {
   return (
     <div id="Header" className="Navigation">
-      <img src={welcome} className="svg-image animate" alt="svg1" />
-      <img src={tictactoe} className="svg-image animate1" alt="svg2" />
-      <img src={welcome} className="svg-image animate2" alt="svg3" />
-      <img src={tictactoe} className="svg-image animate3" alt="svg4" />
+      <img
+        src={welcome}
+        className="svg-image animate"
+        alt="svg1"
+        onMouseOver={props.mouseOverEvent}
+        onMouseLeave={props.mouseOutEvent}
+      />
+      <img
+        src={tictactoe}
+        className="svg-image animate1"
+        alt="svg2"
+        onMouseOver={props.mouseOverEvent}
+        onMouseLeave={props.mouseOutEvent}
+      />
+      <img
+        src={welcome}
+        className="svg-image animate2"
+        alt="svg3"
+        onMouseOver={props.mouseOverEvent}
+        onMouseLeave={props.mouseOutEvent}
+      />
+      <img
+        src={tictactoe}
+        className="svg-image animate3"
+        alt="svg4"
+        onMouseOver={props.mouseOverEvent}
+        onMouseLeave={props.mouseOutEvent}
+      />
     </div>
   );
 };
