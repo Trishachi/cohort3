d6a3ac9c4d90ae5bf306df84c0f1c38dbd37cb9c
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2019-11-27 10:54:36 -0700

    Set up folder structure for Acc Migration

---
+++ react-02/src/components/Accounts/Account.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Account.css";
+
+class Account extends React.Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <div>
+        <h1>Hello From Accounts Component</h1>
+      </div>
+    );
+  }
+}
+
+export default Account;
