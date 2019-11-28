98a775139aed83d4ca737b32b8d5c9a2bad10558
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2019-11-27 23:08:28 -0700

    App HTML Setup-WIP

--- react-02/src/components/Accounts/Account.js
+++ react-02/src/components/Accounts/Account.js
@@ -9,6 +9,20 @@ class Account extends React.Component {
     return (
       <React.Fragment>
         <h1>Hello From Accounts Component</h1>
+        <div id="wrapper" className="container">
+          <div className="row row-grid">
+            <div className="col-md-6">
+              <div id="leftPanel" className="col-md-12">
+                <h4 className="panelTitle">Accounts</h4>
+              </div>
+            </div>
+            <div className="col-md-6">
+              <div id="rightPanel" className="col-md-12">
+                <h4 class="panelTitle">Account Summary</h4>
+              </div>
+            </div>
+          </div>
+        </div>
       </React.Fragment>
     );
   }
