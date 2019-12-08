889290fe2a5aa9314dc5c6151aa04f2509a85cd1
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2019-12-07 22:33:48 -0700

    Set up folder structure for Cities Exercise

--- react-02/src/components/City/City.js
+++ react-02/src/components/City/City.js
@@ -1,5 +1,7 @@
 import React from "react";
+import "../Accounts/Account.css";
 import "./City.css";
+import MsgDisplay from "./MsgDisplay.js";
 
 class City extends React.Component {
   constructor() {
@@ -8,7 +10,23 @@ class City extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <h1>Hello From Cities Component</h1>
+        <h1 className="spacer">Community Management Dashboard</h1>
+        <div id="wrapper" className="container">
+          <div className="row row-grid">
+            <div className="col-md-6">
+              <div id="leftPanel" className="col-md-12">
+                <h4 className="panelTitle">City Roster</h4>
+              </div>
+            </div>
+            <div className="col-md-6">
+              <div id="rightPanel" className="col-md-12">
+                <h4 className="panelTitle">Community Summary</h4>
+                <hr></hr>
+                <MsgDisplay />
+              </div>
+            </div>
+          </div>
+        </div>
       </React.Fragment>
     );
   }
