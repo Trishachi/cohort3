761beb5aa18429698ac03ae1a64d89d47f7ba1fe
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2019-12-11 15:19:42 -0700

    Card Functions error control implemented

--- react-02/src/components/City/CityCard.js
+++ react-02/src/components/City/CityCard.js
@@ -7,7 +7,8 @@ class CityCard extends React.Component {
     this.state = {
       cityCard: this.props.cityCard,
       cardInput: "",
-      citySize: ""
+      citySize: "",
+      cardErrorMessage: ""
     };
     this.handleCardInput = this.handleCardInput.bind(this);
   }
@@ -26,27 +27,45 @@ class CityCard extends React.Component {
       const updatedMovedInCard = this.state.cityCard;
       this.setState({
         cityCard: updatedMovedInCard,
-        cardInput: ""
+        cardInput: "",
+        cardErrorMessage: ""
       });
       this.props.updateCityCard();
       this.showCitySize();
+    } else {
+      this.setState({
+        cardInput: "",
+        cardErrorMessage: "Enter a valid Moved In Number"
+      });
     }
   };
 
   handleMovedOutButton = event => {
     event.preventDefault(event);
     let movedOutAmount = Number(this.state.cardInput);
-    if (movedOutAmount > 0) {
-      this.state.cityCard.movedOut(movedOutAmount);
-      cityFetchFunctions.updateServer(this.state.cityCard);
-      const updatedMovedOutCard = this.state.cityCard;
+    if (movedOutAmount <= 0) {
       this.setState({
-        cityCard: updatedMovedOutCard,
-        cardInput: ""
+        cardInput: "",
+        cardErrorMessage: "Enter a valid Moved Out Number"
       });
-      this.props.updateCityCard();
-      this.showCitySize();
+      return;
+    } else if (movedOutAmount > this.state.cityCard.population) {
+      this.setState({
+        cardInput: "",
+        cardErrorMessage: "Move Out value is larger than current population"
+      });
+      return;
     }
+    this.state.cityCard.movedOut(movedOutAmount);
+    cityFetchFunctions.updateServer(this.state.cityCard);
+    const updatedMovedOutCard = this.state.cityCard;
+    this.setState({
+      cityCard: updatedMovedOutCard,
+      cardInput: "",
+      cardErrorMessage: ""
+    });
+    this.props.updateCityCard();
+    this.showCitySize();
   };
 
   showCitySize = () => {
@@ -106,6 +125,7 @@ class CityCard extends React.Component {
             <div className="spacer">
               Current Population: {population} {this.state.citySize}
             </div>
+            <div className="error spacerTop">{this.state.cardErrorMessage}</div>
           </div>
         </div>
       </Fragment>
