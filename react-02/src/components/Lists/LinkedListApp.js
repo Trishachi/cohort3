a80b37b1beb7357fdd738a2d2f8fd8d79166298c
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2019-12-17 23:00:59 -0700

    Linked List Functions Completed

--- react-02/src/components/Lists/LinkedListApp.js
+++ react-02/src/components/Lists/LinkedListApp.js
@@ -3,21 +3,19 @@ import React from "react";
 import { LinkedList } from "./LinkedListFunctions.js";
 
 const ll = new LinkedList();
-// ll.insertAtFirst(100);
-// ll.removeAtIndex(0);
-// ll.insertAtFirst(200);
-// ll.insertAtFirst(300);
-// ll.insertAtLast(400);
-// ll.insertAtIndex(500, 3);
-// // ll.removeAtIndex(2);
-// // ll.clearList();
-// ll.printListData();
-// // ll.getAtIndex(10);
 
 ll.insertAtFirst("Carrots", 100);
 ll.insertAtFirst("Potatoes", 50);
 ll.insertAtFirst("Berries", 20);
 ll.insertAtLast("Grapes", 70);
+ll.insertAtIndex("Tomatoes", 500, 3);
+// ll.clearList();
+// ll.getAtIndex(3);
+ll.removeAtIndex(3);
+console.log(ll.isListEmpty());
+ll.printListData();
+// ll.removeFromFirst();
+ll.removeFromLast();
 ll.printListData();
 
 function LinkedListApp() {
--- react-02/src/components/Lists/LinkedListFunctions.js
+++ react-02/src/components/Lists/LinkedListFunctions.js
@@ -45,74 +45,107 @@ export class LinkedList {
   }
 
   //insert at index
-  // insertAtIndex(data, index) {
-  //   //if index is out of range - Consider adding to the end
-  //   if (index > 0 && index > this.size) {
-  //     return;
-  //   }
-  //   //if first index ie index is zero
-  //   if (index === 0) {
-  //     this.head = new ListNode(data, this.head);
-  //     return;
-  //   }
-  //   const node = new ListNode(data);
-  //   let current, previous;
+  insertAtIndex(subject, amount, index) {
+    //if index is out of range - Consider adding to the end
+    if (index > 0 && index > this.size) {
+      return;
+    }
+    //if first index ie index is zero
+    if (index === 0) {
+      this.head = new ListNode(subject, amount, this.head);
+      return;
+    }
+    const node = new ListNode(subject, amount);
+    let current, previous;
+    current = this.head;
+    let count = 0;
 
-  //   current = this.head;
-  //   let count = 0;
-
-  //   while (count < index) {
-  //     previous = current; //Node before index
-  //     count++;
-  //     current = current.next; //Node after the index
-  //   }
-  //   node.next = current;
-  //   previous.next = node;
-  //   this.size++;
-  // }
+    while (count < index) {
+      previous = current; //Node before index
+      count++;
+      current = current.forwardNode; //Node after the index
+    }
+    node.forwardNode = current;
+    previous.forwardNode = node;
+    this.size++;
+  }
 
   //Get at index
-  // getAtIndex(index) {
-  //   let current = this.head;
-  //   let count = 0;
+  getAtIndex(index) {
+    let current = this.head;
+    let count = 0;
+    while (current) {
+      if (count === index) {
+        console.log(`${current.subject}: ${current.amount} `);
+      }
+      count++;
+      current = current.forwardNode;
+    }
+    return null;
+  }
 
-  //   while (current) {
-  //     if (count === index) {
-  //       console.log(current.data);
-  //     }
-  //     count++;
-  //     current = current.next;
-  //   }
-  //   return null;
-  // }
+  //check if list is empty
+  isListEmpty() {
+    return this.size === 0;
+  }
+
+  //remove from first
+  removeFromFirst() {
+    if (!this.head) {
+      return;
+    }
+    this.head = this.head.forwardNode;
+    return this.head;
+  }
+
+  // remove from last
+  removeFromLast() {
+    //if list is empty
+    if (!this.head) {
+      return null;
+    }
+    // if only one node in the list
+    if (!this.head.forwardNode) {
+      this.head = null;
+      return;
+    }
+    let previous = this.head;
+    let tail = this.head.forwardNode;
+    while (tail.forwardNode !== null) {
+      previous = tail;
+      tail = tail.forwardNode;
+    }
+    previous.forwardNode = null;
+    return this.head;
+  }
 
   //remove at index
-  // removeAtIndex(index) {
-  //   if (index > 0 && index > this.size) {
-  //     return;
-  //   }
-  //   let current = this.head;
-  //   let previous;
-  //   let count = 0;
-  //   //Remove First
-  //   if (index === 0) {
-  //     this.head = current.next;
-  //   } else {
-  //     while (count < index) {
-  //       count++;
-  //       previous = current;
-  //       current = current.next;
-  //     }
-  //     previous.next = current.next;
-  //   }
-  //   this.size--;
-  // }
+  removeAtIndex(index) {
+    if (index > 0 && index > this.size) {
+      return;
+    }
+    let current = this.head;
+    let previous;
+    let count = 0;
+    //Remove First
+    if (index === 0) {
+      this.head = current.forwardNode;
+    } else {
+      while (count < index) {
+        count++;
+        previous = current;
+        current = current.forwardNode;
+      }
+      previous.forwardNode = current.forwardNode;
+    }
+    this.size--;
+  }
 
   //clear list
-  // clearList() {
-  //   this.head = null;
-  //   this.size = 0;
-  // }
+  clearList() {
+    this.head = null;
+    this.size = 0;
+  }
 
   //print list data
   printListData() {
