16a3360348353d15605e97291322af4f23f51579
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2019-12-18 23:26:27 -0700

    POJO Functions and Tests WIP

--- react-02/src/components/Lists/LinkedListFunctions.js
+++ react-02/src/components/Lists/LinkedListFunctions.js
@@ -10,4 +10,52 @@ export class ListNode {
   }
 }
 
-export class LinkedList {}
+export class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  //Methods to Implement
+  //Helper function to find the current or desired location
+  getAtIndex = index => {
+    let current = this.head;
+    let counter = 0;
+    while (current) {
+      if (counter === index) {
+        console.log(`${current.subject}: ${current.amount} `);
+        return current;
+      }
+      counter++;
+      current = current.forwardNode;
+    }
+  };
+
+  //first
+  first = () => {
+    return this.head;
+  };
+  //last
+  //next
+  //previous
+  //insert
+  insert = (subject, amount, currentIndex) => {
+    //if index is out of range - Consider adding to the end
+    if (currentIndex > 0 && currentIndex > this.size) {
+      return `Index out of range`;
+    }
+    //if index is at zero - ie insert at begining
+    if (currentIndex === 0) {
+      this.head = new ListNode(subject, amount, this.head);
+      return this.head;
+    }
+    //Transveres to find and insert at desired location - after current location
+    const prev = this.getAtIndex(currentIndex - 1);
+    let node = new ListNode(subject, amount);
+    node.forwardNode = prev.forwardNode;
+    prev.forwardNode = node;
+    this.size++;
+    return this.head;
+  };
+  //delete
+  //sumAmounts
+}
--- react-02/src/components/Lists/LinkedlistFunctions.test.js
+++ react-02/src/components/Lists/LinkedlistFunctions.test.js
@@ -1,6 +1,14 @@
 import { ListNode, LinkedList } from "./LinkedListFunctions.js";
 
-test("Linkedlist show function", () => {
+test("ListNode show function", () => {
   const node = new ListNode("Carrot", 20, null);
   expect(node.show()).toBe("Subject: Carrot and Amount: 20");
+});
+
+const newList = new LinkedList();
+test("Testing LinkedList Functions", () => {
+  expect(newList.head).toBe(null);
+  let node1 = newList.insert("Tomatoes", 30, 0);
+  console.log(node1);
+  expect(newList.first()).toBe(node1);
 });
