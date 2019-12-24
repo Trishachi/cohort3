0aecfe71671c9d6b36e32cc4cba3ad2f76182920
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2019-12-24 00:16:46 -0700

    App UI Setup

--- react-02/src/components/Lists/LinkedListApp.js
+++ react-02/src/components/Lists/LinkedListApp.js
@@ -1,12 +1,65 @@
-import React from "react";
+import React, { Fragment } from "react";
+// import "../Accounts/Account.css";
+import "./LinkedList.css";
 
 import { LinkedList } from "./LinkedListFunctions.js";
 
 function LinkedListApp() {
   return (
-    <div>
-      <h1>Welcome to Lists App</h1>
-    </div>
+    <Fragment>
+      <h1 className="spacer">Welcome to Linked List App</h1>
+      <div id="wrapper" className="container">
+        <div className="row row-grid">
+          <div id="leftPanel" className="col-md-12">
+            <h4 className="panelTitle">Linked List Control</h4>
+            <hr />
+            <form>
+              <div className="form-group row">
+                <label htmlFor="subject" className="col-sm-4 col-form-label">
+                  Subject:
+                </label>
+                <div className="col-sm-8">
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="subject"
+                    name="subject"
+                    placeholder="Enter Subject Name"
+                  />
+                </div>
+              </div>
+              <div className="form-group row">
+                <label htmlFor="amount" className="col-sm-4 col-form-label">
+                  Amount:
+                </label>
+                <div className="col-sm-8">
+                  <input
+                    type="number"
+                    className="form-control"
+                    id="amount"
+                    name="amount"
+                    placeholder="Enter an Amount"
+                  />
+                </div>
+              </div>
+              <div className="form-group row">
+                <label className="col-sm-4"></label>
+                <div className="col-sm-8 left-align">
+                  <button id="insert" type="submit" className="btn btn-primary">
+                    Insert
+                  </button>
+                  <button id="delete" type="submit" className="btn btn-primary">
+                    Delete
+                  </button>
+                </div>
+              </div>
+            </form>
+            <hr />
+            <h4 className="panelTitle">Linked List Display</h4>
+          </div>
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
