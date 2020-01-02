b2e5e7ed32dc2d28a74ebe49ec01a442bf20dc42
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2020-01-01 23:29:32 -0700

    SQL Assessment 2 WIP2

--- src/sql/Assessments/Assessment2.sql
+++ src/sql/Assessments/Assessment2.sql
@@ -21,12 +21,28 @@ SELECT name FROM cd.facilities
 WHERE name LIKE '%Tennis%';
 
 -- How can you retrieve the details of facilities with ID 1 and 5? Try to do it without using the OR operator.
--- How can you produce a list of members who joined after the start of September 2012? Return the memid, surname, firstname, and joindate of the members in question.
--- How can you produce an ordered list of the first 10 surnames in the members table? The list must not contain duplicates.
+SELECT * FROM cd.facilities
+WHERE facid IN (1,5);
+
+-- How can you produce a list of members who joined after the start of September 2012?
+-- Return the memid, surname, firstname, and joindate of the members in question.
+SELECT memid, surname, firstname, joindate FROM cd.members
+WHERE joindate > '2012-09-01';
+
+-- How can you produce an ordered list of the first 10 surnames in the members table?
+-- The list must not contain duplicates.
+SELECT DISTINCT surname FROM cd.members
+ORDER by surname ASC
+LIMIT 10;
+
 -- You'd like to get the signup date of your last member. How can you retrieve this information?
+SELECT surname, firstname, joindate FROM cd.members
+ORDER by joindate DESC
+LIMIT 1;
+
 -- Produce a count of the number of facilities that have a cost to guests of 10 or more.
 -- Skip this one, no question for #11.
 -- Produce a list of the total number of slots booked per facility in the month of September 2012. Produce an output table consisting of facility id and slots, sorted by the number of slots.
 -- Produce a list of facilities with more than 1000 slots booked. Produce an output table consisting of facility id and total slots, sorted by facility id.
 -- How can you produce a list of the start times for bookings for tennis courts, for the date '2012-09-21'? Return a list of start time and facility name pairings, ordered by the time.
--- -- How can you produce a list of the start times for bookings by members named 'David Farrell'?
+-- How can you produce a list of the start times for bookings by members named 'David Farrell'?
