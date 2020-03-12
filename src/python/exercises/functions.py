7c81820f1288ac93972a9d6276c9dd2a3f94bc7c
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2020-03-12 00:22:20 -0600

    functions exercise completed

---
+++ src/python/exercises/functions.py
@@ -0,0 +1,17 @@
+# Complete the function by making sure it returns 42. .
+def return_42():
+    print("Return_42 function called")
+    return 42
+
+# Create a function below, called my_function, that takes two arguments and returns the result of its two arguments multiplied together.
+
+
+def my_function(arg1, arg2):
+    result = arg1 * arg2
+    print(result)
+    return result
+
+
+return_42()
+
+my_function(4, 3)
