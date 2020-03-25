3d84f159cc37886defbecf76d9c025f73f882a5c
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2020-03-24 23:58:26 -0600

    Comp 200 TDD WIP

--- src/python/test_syntax.py
+++ src/python/test_syntax.py
@@ -2,10 +2,11 @@ import unittest
 import syntax
 
 
-def fun(x):
-    return x + 1
-
-
 class SyntaxTests(unittest.TestCase):
     def test_hello(self):
         print("Hello World!!!")
+
+    def test_chkVarType(self):
+        self.assertEqual(syntax.var_type(23), "int")
+        self.assertEqual(syntax.var_type("Chisom"), "str")
+        self.assertEqual(syntax.var_type(True), "bool")
