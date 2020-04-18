b6234fd79422173cb0c5cec0e41ffb2019c4f370
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2020-04-17 22:36:17 -0600

    Read directory exercise WIP

---
+++ src/python/Comp220/directory_reader.py
@@ -0,0 +1,11 @@
+import os
+
+
+def read_directory():
+    fpath = input("Enter file path: ")
+    print(fpath)
+
+
+read_directory()
+
+# Try JS File Path - "C:\\code\\cohort3\\src\\javascript\\2019-11-01-oop"
