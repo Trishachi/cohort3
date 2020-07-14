ac034584e76e942d70d191b569fd58a223cd55b9
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2020-07-13 23:07:55 -0600

    working on dict

--- src/python/Comp230/playground.py
+++ src/python/Comp230/playground.py
@@ -1,21 +1,43 @@
 from openpyxl import load_workbook
-from openpyxl.worksheet.datavalidation import DataValidation
 
-wb = load_workbook(filename='store_data.xlsx')
+wb = load_workbook(filename='store_data.xlsx', data_only=True)
 
-wb_sheet = wb.active
-ws_invoice = wb['Invoices']
-wb.active = ws_invoice
-# Change current active worksheet to invoice worksheet
-invoice_sheet = wb.active
-c1 = invoice_sheet['A1'].value
+# Load worksheets
+customer_sheet = wb['Customers']
+products_sheet = wb['Products']
+invoice_sheet = wb['Invoices']
+invoiceItems_sheet = wb['Invoice_details']
+
+# for i in range(1, invoice_sheet.max_row):
+#     header = invoice_sheet.cell(row=i, column=1).value
+#     # invoice_id = invoice_sheet.cell(row=i, column=1).value
+#     # customer_id = invoice_sheet.cell(row=i, column=2).value
+#     # order_date = invoice_sheet.cell(row=i, column=3).value
+#     # invoice_amt = invoice_sheet.cell(row=i, column=4).value
+#     # shipping_add = invoice_sheet.cell(row=i, column=5).value
+#     # print("{:.2f}".format(invoice_amt))
+#     print(header)
+
+for i in range(1, invoice_sheet.max_row):
+    for j in range(1, invoice_sheet.max_column):
+        cell_obj = invoice_sheet.cell(row=i, column=j)
+        print(cell_obj.value, end=' | ')
+    print('\n')
+
+
 # Get invoice id to be generated as input from user
 # target_invoice = input('Enter Invoice Id: ')
+# try:
+#     target_invoice = int(target_invoice)
+# except:
+#     print("Invalid input, terminating...")
+# if target_invoice < 101:
+#     print("Invalid Invoice ID, terminating...")
 
-# Iterate over the worksheet rows
-for row in invoice_sheet.iter_rows(invoice_sheet.min_row, invoice_sheet.max_row):
-    for cell in row:
-        print(cell.value)
+
+# for row in invoice_sheet.iter_rows(invoice_sheet.min_row, invoice_sheet.max_row):
+#     for cell in row:
+#         print(cell.value)
 
 # for row in invoice_sheet.iter_rows(min_row=1, max_col=1, values_only=True):
 #     for value in row:
@@ -24,7 +46,6 @@ for row in invoice_sheet.iter_rows(invoice_sheet.min_row, invoice_sheet.max_row)
 # Find row of the invoice id provided
 # Collect all data in the row
 # Access the cells required for display on the invoice file
-
 
 # print(c1)
 # print(target_invoice)
