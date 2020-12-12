3f0560dec46569886698c3d5e8e213a8539c2c74
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2020-12-11 22:49:30 -0700

    authentication route added

--- src/python/rest-api-tutorial/section4/code/app.py
+++ src/python/rest-api-tutorial/section4/code/app.py
@@ -1,26 +1,41 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
 
 app = Flask(__name__)
+app.secret_key = "chisom"
 api = Api(app)
 
+jwt = JWT(app, authenticate, identity)  # JWT creates a new end point /auth
 
 items = []
 
 
 class Item(Resource):
+    @jwt_required()
     def get(self, name):
-        for item in items:
-            if item['name'] == name:
-                return item
-        return{'item': None}, 404
+        # Gives us the first item matched by the filter function
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        return{'item': item}, 200 if item else 404
 
     def post(self, name):
-        item = {'name': name, 'price': 12.00}
+        if next(filter(lambda x: x['name'] == name, items), None):
+            return {'message': "An item with name '{}' already exists.".format(name)}, 400
+
+        data = request.get_json()
+        item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201
 
 
-api.add_resource(Item, '/item/<string:name>')
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
 
-app.run(port=5000)  # default port
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+
+app.run(port=5000, debug=True)  # default port
